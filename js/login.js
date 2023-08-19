581d7e668f86770b7dd6fbcec296b56f675b929d
JoaquinRodriguez07 <joaquinrzg@gmail.com>
2023-08-19 16:25:17 -0300

    Updates
    agregue el js del login y un boton de cerrar sesion en el index

---
+++ js/login.js
@@ -0,0 +1,24 @@
+
+document.getElementById('formulario').addEventListener('submit', (e) =>{
+ 
+    e.preventDefault()
+    const pass = document.getElementById('pass').value
+    const mail = document.getElementById('mail').value
+
+    console.log(pass)
+    console.log(mail)
+
+    localStorage.setItem('Email', mail);
+    localStorage.setItem('pass', pass);
+
+    console.log(localStorage)
+
+
+    alert('Registrado con éxito!')
+
+    window.location.href = 'index.html'
+
+})
+
+
+
