ca8d74f1bcc0e455cc85efaee2d837cd38b16fa0
JoaquinRodriguez07 <joaquinrzg@gmail.com>
2023-09-02 19:05:56 -0300

    Solucion de Js de las imagenes
    Faltaban algunas funciones, y solucion en cambio de nombre de variables

--- js/index.js
+++ js/index.js
@@ -12,7 +12,20 @@ const mailvalidation = localStorage.getItem('Email');
 console.log(mailvalidation);
 console.log(passvalidation);
 
-
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
 
 
 
