12777f353c2bc91eba15dcaeeb72040716192eea
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-11-09 21:41:40 -0300

    Agrego la funcionalidad para validar los campos y guardarlos en el local

--- js/my-profile.js
+++ js/my-profile.js
@@ -20,3 +20,44 @@ botonImg.addEventListener('click',()=>{
     console.log(ImputImg.value)
 
 })
+
+const botonGuardar = document.getElementById("mandar");
+const inputPrimerNombre = document.getElementById("primer-nombre");
+const inputPrimerApellido = document.getElementById("primer-apellido");
+const inputEmail = document.getElementById("email");
+const inputSegundoNombre = document.getElementById("segundo-nombre");
+const inputSegundoApellido = document.getElementById("segundo-apellido");
+const inputCelular = document.getElementById("celular");
+
+botonGuardar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    if (
+        inputPrimerNombre.value.trim() === "" ||
+        inputPrimerApellido.value.trim() === "" ||
+        inputEmail.value.trim() === "" ||
+        inputCelular.value.trim() === ""
+      ) {
+        alert("Por favor, complete los campos obligatorios");
+        return;
+      }
+
+      const perfil = {
+        primerNombre: inputPrimerNombre.value.trim(),
+        sgundoNombre: inputSegundoNombre.value.trim(),
+        pimerApellido: inputPrimerApellido.value.trim(),
+        segundoApellido: inputSegundoApellido.value.trim(),
+        email: inputEmail.value.trim(),
+        celular: inputCelular.value.trim(),
+
+      };
+
+      localStorage.setItem("perfilUsuario", JSON.stringify(perfil));
+      alert("Datos guardados con exito")
+});
+
+
+
+
+
+
